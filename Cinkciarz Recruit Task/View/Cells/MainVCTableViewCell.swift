6d82bcfa63d1964bdd176ab5261c4ffc6222d4b6
FilipGrudzinski <grudzinski.filip@gmail.com>
2019-03-22 00:01:50 +0100

    Storyboard set, segue added, Agency Cell layout set

---
+++ Cinkciarz Recruit Task/View/Cells/MainVCTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MainVCTableViewCell.swift
+//  Cinkciarz Recruit Task
+//
+//  Created by Filip on 21/03/2019.
+//  Copyright © 2019 Filip. All rights reserved.
+//
+
+import UIKit
+
+class MainVCTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Cinkciarz Recruit Task/View/Cells/RocketLaunchVCTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RocketLaunchVCTableViewCell.swift
+//  Cinkciarz Recruit Task
+//
+//  Created by Filip on 21/03/2019.
+//  Copyright © 2019 Filip. All rights reserved.
+//
+
+import UIKit
+
+class RocketLaunchVCTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
