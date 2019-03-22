6f2e69ffe467440ec454c517854bcb058ab7c8cb
FilipGrudzinski <grudzinski.filip@gmail.com>
2019-03-22 12:30:36 +0100

    agencyVC tableview functions

--- Cinkciarz Recruit Task/Controller/AgencyViewController.swift
+++ Cinkciarz Recruit Task/Controller/AgencyViewController.swift
@@ -8,23 +8,32 @@
 
 import UIKit
 
-class AgencyViewController: UIViewController {
+class AgencyViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+   
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "Back", style: .plain, target: nil, action: nil)
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 90
+        
     }
-    */
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! MainVCTableViewCell
+        
+        return cell
+    }
+    
+   
+    
 
 }
